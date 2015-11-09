8fda864d6a8487665873f83cb43269fc2018f194
Davy Jones <Davy@localhost.localdomain>
2015-11-09 14:35:59 +0100

    Added Static controller with routes for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#index'
+
+  get '/index', to:'static#index'
+  get '/login', to:'static#login'
+  get '/logout', to:'static#logout'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
